7b3580a6359056f2a5f9b6959ea2fb11cd71bbaa
Tim <randomthecoder1557@gmail.com>
2025-09-21 12:29:43 -0300

    add global colors

--- src/globals/globals.cpp
+++ src/globals/globals.cpp
@@ -3,3 +3,8 @@
 const int WINDOW_WIDTH = 1280;
 const int WINDOW_HEIGHT = 800;
 const char* WINDOW_TITLE = "Pong";
+
+const Color Green = Color{38, 185, 154, 255};
+const Color DarkGreen = Color{20, 160, 133, 255};
+const Color LightGreen = Color{129, 204, 133, 255};
+const Color Yellow = Color{243, 213, 91, 255};
--- src/globals/globals.h
+++ src/globals/globals.h
@@ -1,5 +1,11 @@
 #pragma once
+#include <raylib.h>
 
 extern const int WINDOW_WIDTH;
 extern const int WINDOW_HEIGHT;
 extern const char* WINDOW_TITLE;
+
+extern const Color Green;
+extern const Color DarkGreen;
+extern const Color LightGreen;
+extern const Color Yellow;
